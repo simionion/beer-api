a988ab1745c46c084e2e070926d7fb2ca8386c0f
Simion Ion <52240502+simionion@users.noreply.github.com>
2024-06-02 20:56:33 +0300

    Added schema props

--- components/beer/beer.js
+++ components/beer/beer.js
@@ -1,10 +1,13 @@
 export function BeerTemplate(beer) {
     return `
-        <div class="beer_container" id="beer-${beer.id}" tabindex="0" role="article">
-            <img class="beer_image" src="${beer.image_url}" alt="${beer.name}">
-            <h2 class="beer_title">${beer.name}</h2>
-            <p class="beer_ibu">IBU: ${beer.ibu}</p>
-            <p class="beer_abv">${beer.abv}%</p>
+        <div class="beer_container" id="beer-${beer.id}" tabindex="0" aria-label="Beer named: ${beer.name}, with ${beer.ibu} bitterness units, and ${beer.abv}% alcohol." 
+          itemscope itemtype="https://schema.org/LiquorStore" aria-describedby="beer-${beer.id}-description" aria-labelledby="beer-${beer.id}-title"
+        >
+            <img class="beer_image" src="${beer.image_url}" alt="${beer.name}" itemprop="image">
+            <h2 class="beer_title" id="beer-${beer.id}-title" itemprop="name">${beer.name}</h2>
+            <p class="beer_description visually-hidden" id="beer-${beer.id}-description" itemprop="description">${beer.description}</p>
+            <p class="beer_ibu" title="The International Bitterness Units of the beer">IBU: ${beer.ibu}</p>
+            <p class="beer_abv" title="The alcohol by volume percentage of the beer">${beer.abv}%</p>
         </div>
     `;
 }
--- components/modal_beer/modal_beer.js
+++ components/modal_beer/modal_beer.js
@@ -4,12 +4,12 @@ export function BeerModalTemplate(beer, order_options) {
     const ordering_options_dropdown = create_order_dropdown(order_options);
 
     return `
-        <div class="modal_beer_container" >
-           <div class="modal_beer_img_prewrapper" tabindex="0" role="article">
+        <article class="modal_beer_container" >
+           <div class="modal_beer_img_prewrapper" tabindex="0" aria-label="Beer named: ${beer.name}, with ${beer.ibu} bitterness units, and ${beer.abv}% alcohol. Description: ${beer.description}">
                 <span class="modal_beer_img_wrapper">
                    <img class="modal_beer_image" src="${beer.image_url}" alt="${beer.name}" />
-                    <p class="modal_beer_ibu">IBU<br/>${beer.ibu}</p>
-                    <p class="modal_beer_abv">${beer.abv}%</p>
+                    <p class="modal_beer_ibu" title="The International Bitterness Units of the beer">IBU<br/>${beer.ibu}</p>
+                    <p class="modal_beer_abv" title="The alcohol by volume percentage of the beer">${beer.abv}%</p>
                 </span>
           </div>
             <p class="modal_beer_description">${beer.description}</p>
@@ -17,6 +17,6 @@ export function BeerModalTemplate(beer, order_options) {
             <h2 class="modal_beer_title ">${beer.name}</h2>
              ${order_options.length ? ordering_options_dropdown : "Out of stock"}
             </div>
-        </div>
+        </article>
     `;
 }
