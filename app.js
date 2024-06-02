f3df9d770b83b87575892376422c433c82db6b0b
Simion Ion <52240502+simionion@users.noreply.github.com>
2024-06-02 12:13:44 +0300

    Refactored beer modal to module

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import {create_order_dropdown} from './components/dropdown/dropdown.js';
 import {create_modal_context} from './components/modal/modal.js';
+import {BeerModalTemplate} from './components/modal_beer/modal_beer.js';
 
 /**
  * @typedef {Object} Beer
@@ -35,27 +35,6 @@ function BeerTemplate(beer) {
     `;
 }
 
-
-function BeerModalTemplate(beer, order_options) {
-    const ordering_options_dropdown = create_order_dropdown(order_options);
-
-    return `
-        <div class="modal_beer_container" >
-           <div class="modal_beer_img_prewrapper" tabindex="0" role="article">
-                <span class="modal_beer_img_wrapper">
-                   <img class="modal_beer_image" src="${beer.image_url}" alt="${beer.name}" />
-                    <p class="modal_beer_ibu">IBU<br/>${beer.ibu}</p>
-                    <p class="modal_beer_abv">${beer.abv}%</p>
-                </span>
-          </div>
-            <p class="modal_beer_description">${beer.description}</p>
-            <div class="modal_beer_footer">
-            <h2 class="modal_beer_title ">${beer.name}</h2>
-             ${ordering_options_dropdown}
-            </div>
-        </div>
-    `;
-}
 
 function run_on_enter(callback) {
     return function (e) {
---
+++ components/modal_beer/modal_beer.js
@@ -0,0 +1,22 @@
+import {create_order_dropdown} from "../dropdown/dropdown.js";
+
+export function BeerModalTemplate(beer, order_options) {
+    const ordering_options_dropdown = create_order_dropdown(order_options);
+
+    return `
+        <div class="modal_beer_container" >
+           <div class="modal_beer_img_prewrapper" tabindex="0" role="article">
+                <span class="modal_beer_img_wrapper">
+                   <img class="modal_beer_image" src="${beer.image_url}" alt="${beer.name}" />
+                    <p class="modal_beer_ibu">IBU<br/>${beer.ibu}</p>
+                    <p class="modal_beer_abv">${beer.abv}%</p>
+                </span>
+          </div>
+            <p class="modal_beer_description">${beer.description}</p>
+            <div class="modal_beer_footer">
+            <h2 class="modal_beer_title ">${beer.name}</h2>
+             ${ordering_options_dropdown}
+            </div>
+        </div>
+    `;
+}
